7b11408a7ea26e70ae3d7f943f4dfbaa890c66b2
Аюб Дудуев (iOS) <aub.duduev.1989@gmail.com>
2021-07-03 07:28:38 +0300

    Программа, которая на вход получает значения X в заданном диапазоне, а на выходе выдает значения площадей окружности радиуса X и квадрата со стороной X и сравнивает их величины.

--- app/src/main/java/com/example/swiftbookapp/MainActivity.kt
+++ app/src/main/java/com/example/swiftbookapp/MainActivity.kt
@@ -2,13 +2,48 @@ package com.example.swiftbookapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import java.lang.Math.PI
+import java.lang.Math.pow
+import kotlin.math.sqrt
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.basket_layout)
 
+    val radiusCircle: Double = 5.0
+    val sideSquare  : Double = 5.0
+    val circleArea = this.areaOfCircleCalculator(radiusCircle)
+    val squareArea = this.areaOfSquareCalculator(sideSquare)
+
+    val comparisonResult = this.compareTheAreaOfSquareAndCircle(squareArea, circleArea)
 
   }
+  //S = π * r2
+  private fun areaOfCircleCalculator(x: Double): Double {
+    val result = PI * (x * x)
+    return result
+  }
+  //S = a2
+  private fun areaOfSquareCalculator(x: Double): Double {
+    val result = x * x
+    return result
+  }
 
+  private fun compareTheAreaOfSquareAndCircle(square: Double, circle: Double): String {
+
+    val result = when (true) {
+
+      square == circle -> "Площади равны"
+
+      square > circle -> "Площадь квадрата больше"
+
+      square < circle -> "Площадь круга больше"
+
+      else -> "Резултат не известен"
+
+    }
+    return result
+  }
 }
